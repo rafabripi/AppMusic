b7ffe2a5749caedfb64f483b510d0027f0ea77ab
RafaBriPi <rafael@glouu.com>
2019-09-13 17:18:59 -0500

    new

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang='pug' >
   #app
-    img(src='./assets/logo.png')
+    img(src='https://rafabripi.github.io/AppMusic/assets/logo.png')
     h1 {{ msg }}
     select(v-model='selectedCountry' )
       option(v-for='country in countries' v-bind:value='country.value') {{ country.name }}
